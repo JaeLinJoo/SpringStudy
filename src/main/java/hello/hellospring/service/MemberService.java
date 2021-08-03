a02e22dcd4121e39958e6addf59bf648b2fd56f0
jell <jl1096@ajou.ac.kr>
2021-08-03 20:12:10 +0900

    회원 서비스 조회 시간 측정 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,9 +25,19 @@ public class MemberService {
     */
     public Long join(Member member){
 
-        validateDuplicateMember(member); // 중복 회원 검증증
-       memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+timeMs+"ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -40,8 +50,16 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+    public List<Member> findMembers() {
+
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = "+timeMs+"ms");
+        }
     }
 
     public  Optional<Member> findOne(Long memberId){
